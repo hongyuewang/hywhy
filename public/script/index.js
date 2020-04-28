b3771dd43cec1549f6f87093f665f9653e203496
Hong Yue Wang <hongyuew26@gmail.com>
2020-04-28 16:56:19 -0400

    Added scroll effect to navbar & resume now opens in new tab

---
+++ public/script/index.js
@@ -0,0 +1,15 @@
+/* useless for now
+function getPositionX(element) {
+    var rect = element.getBoundingClientRect();
+    return rect.x;
+}
+
+function getPositionY(element) {
+    var rect = element.getBoundingClientRect();
+    return rect.y;
+}
+
+
+function scrollToAbout() {
+    window.scrollTo(getPositionX(document.getElementById('about')), getPositionY(document.getElementById('about')));
+} */
