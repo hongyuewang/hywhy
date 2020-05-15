0d9a08e53403d5415a0cce46a9da3161b73a3449
Hong Yue Wang <hongyuew26@gmail.com>
2020-05-15 12:55:12 -0400

    Removed white-colored sections & desaturated picture

--- public/script/index.js
+++ public/script/index.js
@@ -1,15 +1,1 @@
-/* useless for now
-function getPositionX(element) {
-    var rect = element.getBoundingClientRect();
-    return rect.x;
-}
 
-function getPositionY(element) {
-    var rect = element.getBoundingClientRect();
-    return rect.y;
-}
-
-
-function scrollToAbout() {
-    window.scrollTo(getPositionX(document.getElementById('about')), getPositionY(document.getElementById('about')));
-} */
